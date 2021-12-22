631a9382164c7ec903fd8a956c927fd15ed74f7c
Daniel Kim <kkdh1109@gmail.com>
2021-12-22 11:03:27 -0500

    add styles to links using @layer/@apply css property

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Head>
+        <title>Google</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        {/* aligns items on vertical-axis */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+        </div>
+      </header>
+    </div>
+  );
 }
