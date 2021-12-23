17245b65b490563015b70abc44437aa1e181b0d3
Daniel Kim <kkdh1109@gmail.com>
2021-12-23 11:10:52 -0500

    create a search page and part of header component including img and search form

---
+++ components/Header.js
@@ -0,0 +1,50 @@
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+
+function Header() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
+  return (
+    <header>
+      <Image
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"
+        height={40}
+        width={120}
+        className="cursor-pointer"
+        onClick={() => router.push("/")}
+      />
+
+      <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+        <input
+          ref={searchInputRef}
+          type="text"
+          className="flex-grow w-full focus:outline-none"
+        />
+        <XIcon
+          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+          onClick={() => {
+            searchInputRef.current.value = "";
+          }}
+        />
+        <MicrophoneIcon className="h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+        <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
+        <button hidden type="submit" onClick={search}>
+          Search
+        </button>
+      </form>
+    </header>
+  );
+}
+
+export default Header;
