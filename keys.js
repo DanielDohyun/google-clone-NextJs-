c6535689794f2725a629481059505c9a85be9719
Daniel Kim <kkdh1109@gmail.com>
2021-12-24 13:06:58 -0500

    add fetch call to get google search result and save res as dummy data

---
+++ keys.js
@@ -0,0 +1,3 @@
+// API Key =allows to use google api - https://developers.google.com/custom-search/v1/using_rest
+
+// Context key - https://cse.google.com/cse/create/new
--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Response from "../components/Response";
 
-function Search() {
+function Search({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -14,3 +16,20 @@ function Search() {
 }
 
 export default Search;
+
+// if you include getserverside function => treats this page as a server side rendered page
+export async function getServerSideProps(context) {
+  const useDummyData = true;
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}`
+      ).then((res) => res.json());
+
+  //after server has rendered => pass the res to client
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
